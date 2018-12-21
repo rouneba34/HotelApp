ed87f72ef617f12491275f3f67c9089a489f8a07
Nana N'GUISSAN <macbookpro@MACBOOKs-MacBook-Pro.local>
2018-12-21 19:59:46 +0000

    Nouveau commit

---
+++ src/java/org/ucad/User.java
@@ -0,0 +1,32 @@
+package java.org.ucad.sir;
+
+public class User {
+	private String nom;
+	private String prenom;
+	
+	public User(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+
+}
+
